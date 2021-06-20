832a9d0f84b04108c8b8533f4bb687ccdccd562a
César Salcedo <cesar.salcedo@utec.edu.pe>
2021-06-20 17:40:38 -0500

    Added file reading to main.cpp

--- lab05/main.cpp
+++ lab05/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
+#include "syntax_analyzer.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    // Validate arguments
+    if (argc != 2) {
+        cerr << "error: Invalid number of arguments. Usage:" << argv[0] << " <grammar>" << endl;
+        return 1;
+    }
+
+    // Receive file
+    string grammar_filename(argv[1]);
+    fstream grammar_file(grammar_filename, ios::in);
+
+    if (!grammar_file.is_open()) {
+        cerr << "error: Could not open grammar file '" << grammar_filename << "'." << endl;
+        return 1;
+    }
+
+    // Built grammar
+    syntax_analyzer analyzer(grammar_file);
+
+    // Receive input
+    string input;
+
+    while (getline(std::cin, input))
+        cout << analyzer.parse(input) << endl;
 
     return 0;
 }
